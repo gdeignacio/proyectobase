041e99000e56e6934867a4e5a0275cf9d3319c1d
gdeignacio <gdeignacio@gmail.com>
2016-07-11 21:30:18 +0200

    Initial

---
+++ proyectobase/proyectobase-ejb/src/main/java/es/caib/proyectobase/Version.java
@@ -0,0 +1,10 @@
+package es.caib.proyectobase;
+/**
+* Código autogenerado por la Version.java.template
+*/
+public final class Version {
+    public static final String VERSION="0.0.1-SNAPSHOT";
+    public static final String BUILDTIME="05/04/2016 13:08";
+    public static final String SVN_REVISION="-1";
+    public static final String JDK_VERSION="1.7.0_71-b14";
+}
